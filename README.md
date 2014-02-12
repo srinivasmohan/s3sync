7febc6ca935083bf37385b0f7f56f612850fc1e0
Srinivasan Mohan <srinivas@onepwr.org>
2014-02-12 10:27:10 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,84 @@
+S3 Sync
+=======
+
+Scenario: Have a large S3 bucket that you want to sync to another bucket.
+
+Typical Solutions -
+Use s3cmd / s3sync sort of tools - These worked very well for smaller buckets but not for large ones (with 300000+ objects) as they appeared to run one object at a time... Hence this ruby script. (s3cmd was done with about 20000 objects after having run from a ec2 instance for over 20 hours!)
+
+With this Jruby based script, I could get my ~300000 objects bucket (approx 450GB of objects) synced from a US West (oregon only) bucket synced to:
+* A US Standard bucket in under 4 hours.
+* A EU (Ireland) bucket in about 6 hours. (On a different note, that means AWS pushed 75GB/hour i.e. ~200Mbps or higher from US-EU - Dedicated links for AWS use alone?)
+
+And subsequent syncs (every hour) run in under 10 minutes. The code could do with a lot of cleanup but the gist is when copying between S3 buckets under the same account, its best to use S3 `copy_to` rather than fetch and upload each object - See [S3Object copy_to](http://docs.aws.amazon.com/AWSRubySDK/latest/AWS/S3/S3Object.html#copy_to-instance_method) for details.
+
+Caveats
+=======
+
+This is code written by a sysad to run quick :-)
+
+You could sync in two modes, based on -
+* Existence of keys (compare list of keys on src vs keys on destination buckets and ONLY copy objects that exist on Source but not at Destination)
+* Use the Etag of objects at Src and Dest to compare and push if Etag is different.
+
+The former is very quick if you run it frequently enough - In my case, objects pushed to S3 never get changed so this works for me. However there may be cases where the S3 object may be modified too. The Etag comparison comes handy then as it does a copy if the key does'nt exist on Destination and also copies if the content-length/etag are different.
+
+Keep in mind that for most cases, the Etag returned by S3 for an object is equivalent to the objects md5sum - However this is not true for multi-part uploads. (If the etag has a '-' followed by some number that was a mutipart upload). So comparing Etag for multipart upload files does'nt make sense. Most of my object are under 400MB so I set `:s3_multipart_threshold` to 400MB - YMMV. Updating a S3 object (e.g. add a tag) forces S3 to recompute the Etag for that object so thats a crude way to have the Etag for a multi-part uploaded object changed to its true md5sum. However S3 only guarantees the presence of an Etag - no promises that this needs to be a md5sum, so that may very well change.
+
+Also, AWS charges for S3 access from outside EC2... If I were to run a normal fetch/upload inside EC2 to S3 it should ideally cost me nothing (except performance). Use of `copy_to` means this happens internally with S3 itself making the client fetch/upload not necessary - Not sure how/if you would get billed for that. 
+
+Setup
+=====
+
+I needed to be able to run multiple threads concurrently to speed up the sync process (My use case for this was to backup a US bucket to EU and vice versa to minimize local latency and not have to come across the pond). Due to Ruby MRI / Pythons [GIL mechanism](http://en.wikipedia.org/wiki/Global_Interpreter_Lock), running parallel threads with stock Ruby was'nt possible so I picked Jruby (Code in Ruby but run in JVM).
+
+My test boxes were Ubuntu so your setup may vary slightly:
+
+```
+apt-get install openjdk-7-jdk openjdk-7-jre openjdk-7-jre-headless openjdk-7-jre-lib
+cd /opt && wget http://jruby.org.s3.amazonaws.com/downloads/1.7.10/jruby-bin-1.7.10.tar.gz
+tar zxvf jruby-bin-1.7.10.tar.gz
+export PATH=/opt/jruby-1.7.10/bin:$PATH
+
+root@wh1-use:~/parallel-s3sync# jruby -v
+jruby 1.7.10 (1.9.3p392) 2014-01-09 c4ecd6b on OpenJDK 64-Bit Server VM 1.7.0_51-b00 [linux-amd64]
+
+#Get the aws-sdk gem installed.
+jruby -S gem install aws-sdk
+
+#Check
+root@wh1-use:~/parallel-s3sync# jruby -S gem list --local | grep aws-sdk
+aws-sdk (1.33.0)
+
+
+```
+
+
+Sample Config
+=============
+
+Script expects `s3auth.yml` in the same folder as the script.
+
+```
+
+---
+aws_key: YOURKEY
+aws_secret: YOURSECRET
+s3src:
+- YOUR_USWEST_BUCKET
+- s3-us-west-2.amazonaws.com
+s3dest:
+- YOUR_DST_BUCKET
+- s3.amazonaws.com
+prefix: uploads
+usemd5: true
+
+```
+
+Where -
+* `s3src` and `s3dest` are arrays of `['BUCKETNAME','bucket-endpoint']` - Bucket endpoints as listed in [AWS Docs](http://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region)
+* `prefix` - Is the toplevel prefix to sync from Source to Destination buckets
+* `usemd5` - Is true/false - When false, simply copy keys in SRC but not in DST to DST. When True also look at etag/content length from a `HEAD` on the objects.
+
+Then run `nohup /pathto/s3sync.rb` and thats it!
+
---
+++ s3sync.rb
@@ -0,0 +1,176 @@
+#!/opt/jruby-1.7.10/bin/jruby
+
+require "rubygems"
+require "yaml"
+require "json"
+require "aws-sdk"
+
+require "java"
+java_import 'java.util.concurrent.Callable'
+java_import 'java.util.concurrent.FutureTask'
+java_import 'java.util.concurrent.LinkedBlockingQueue'
+java_import 'java.util.concurrent.ThreadPoolExecutor'
+java_import 'java.util.concurrent.TimeUnit'
+
+MAXTHREADS=75
+
+def logmsg(msg)
+	puts "#{Time.now.strftime('%Y%m%d.%H:%M:%S')} - #{msg}" unless msg.nil?
+end
+
+def getlist(bucket,prefixstr)
+	logmsg("Fetching object list from bucket #{bucket.name} (Prefix #{prefixstr})")
+	objlist=Hash.new
+	size=0
+	bucket.objects.with_prefix(prefixstr).each do |x|
+		objlist[x.key]=nil
+	end
+	logmsg("#{bucket.name} (Prefix #{prefixstr}) => #{objlist.keys.length} items")
+	objlist
+end
+
+def getattrib(obj)
+	attribhash={}
+	begin
+		obj.head.delete_if{ |k,v| !(k==:etag || k==:content_length)}.each_pair {|k,v| attribhash[k]=v }	
+	rescue AWS::S3::Errors::NoSuchKey => e	
+		attribhash={}
+	end	
+	attribhash
+end
+
+def copy(srcb,dstb,keylist,usemd5=false) #keylist is array of [key,obj.head]
+	return "NothingToDo" unless keylist.length>0
+	count=keylist.length
+	synced=0	
+	tosync=0
+	keylist.each do |y|
+		key=y[0]
+		srcobj=srcb.objects[key]
+		srchead=getattrib(srcobj)
+		reason="missing-at-dst"
+		ifCopy=false
+		if usemd5
+			dstobj=dstb.objects[key]
+			dsthead=getattrib(dstobj)
+			ifMissing=(dsthead.keys.length==0) ? true: false
+			sizechanged=srchead.has_key?(:content_length) && dsthead.has_key?(:content_length) && srchead[:content_length].to_i!=dsthead[:content_length].to_i
+			noMatch=dsthead.has_key?(:etag) && srchead.has_key?(:etag) && dsthead[:etag]!=srchead[:etag] && sizechanged ? true: false
+			ifCopy=(ifMissing || noMatch)
+			reason="md5-mismatch src:#{srchead[:etag]} dst:#{dsthead[:etag]}" if noMatch
+		else #If we were called with usemd5=false, we were already suppiled a list of keys on src but not in dst. So copy.
+			ifCopy=true
+		end	
+		#logmsg("RAW|#{key}|#{srchead[:content_length]}|#{srchead[:etag]}")
+		next unless ifCopy
+		#logmsg("TO-COPY: #{key} [#{reason}] #{srchead[:content_length]}Bytes")
+		tosync+=1
+		size=srchead[:content_length]
+		begin	
+			srcobj.copy_to(key, {:bucket => dstb})
+		rescue Exception => e
+			logmsg("Key: #{key}, Reason=[#{reason}], Error: #{e.inspect}")
+			next
+		end
+		synced+=1		
+		logmsg("Key: #{key} Reason=[#{reason}], Copied OK (#{size} Bytes)") 
+	end
+	return (tosync > 0 ? "Synced #{synced}/#{usemd5 ? tosync : count} objects":"NothingToDo - NO sync'able objects")
+end
+
+Conffile=File.dirname(__FILE__)+"/s3auth.yml"
+
+class Syncer
+	include Callable
+
+	def initialize(srcb,dstb,objlist,usemd5=false)
+		@srcb=srcb
+		@dstb=dstb
+		@list=objlist
+		@usemd5=usemd5
+	end
+	
+	def call
+		retstr=nil
+		begin
+			retstr="ThreadFinished: "+copy(@srcb,@dstb,@list,@usemd5)
+		rescue Exception => e
+			retstr="ThreadError: #{e.inspect}"
+		ensure
+			logmsg(retstr)
+		end
+	end
+
+end
+
+begin
+  confighash=YAML::load(File.open(Conffile))
+rescue Exception => e
+  abort "Could not parse YAML file (#{Conffile}) - #{e.inspect}"
+end
+
+src=confighash['s3src'][0]
+dest=confighash['s3dest'][0]
+logmsg("START: Sync from bucket #{src} [#{confighash['s3src'][1]}] TO #{dest} [#{confighash['s3dest'][1]}]")
+AWS.config({
+  :region => 'us-west-2',
+	:use_ssl => false, #no ssl maybe faster?
+	:s3_multipart_threshold => 400*1024*1024 #No multipart uploads for objects under this size - Etga gets messay when multipart uploads are done :(
+})
+
+usemd5=confighash.has_key?('usemd5') && confighash['usemd5'] ? true: false
+logmsg("Comparison: MD5 sums WILL #{usemd5 ? '': 'NOT'} BE compared! - Use of md5sums/etags is slower but safer!!!")
+s3svc1=AWS::S3.new(:access_key_id => confighash['aws_key'], :secret_access_key => confighash['aws_secret'], :s3_endpoint => confighash['s3src'][1])
+s3svc2=AWS::S3.new(:access_key_id => confighash['aws_key'], :secret_access_key => confighash['aws_secret'], :s3_endpoint => confighash['s3dest'][1])
+
+srcbucket=s3svc1.buckets[src]
+dstbucket=s3svc2.buckets[dest]
+
+srclist=getlist(srcbucket,confighash['prefix'])
+
+if !usemd5 #Filter by existence alone
+	dstlist=getlist(dstbucket,confighash['prefix'])
+	srclist.keys.each do |x|
+		srclist.delete(x) if dstlist.has_key?(x)		
+	end
+	logmsg("PruneList: No-MD5-Check - SRC has #{srclist.keys.length} item(s) to sync to DST")
+else
+	logmsg("PruneList: MD5-Check - SRC has #{srclist.keys.length} item(s) - Each will be checked against DST for md5/etag")
+end
+
+if srclist.keys.length==0
+	logmsg("ENDED: Nothing to sync!")
+	exit 0
+end
+copyobjects=srclist.keys.each.map { |x| [ x, srclist[x]] }
+
+tsnow=Time.now
+numObjectsPerThread=(copyobjects.length<= MAXTHREADS) ? copyobjects.length : copyobjects.length/MAXTHREADS
+brokenUp=copyobjects.each_slice(numObjectsPerThread).to_a
+thrct=brokenUp.length
+
+logmsg("START: Sync via #{thrct} threads with upto #{numObjectsPerThread} objects per thread.")
+#Executor pool 
+executor = ThreadPoolExecutor.new(
+	MAXTHREADS, # #pool_treads
+	MAXTHREADS, # max pool_threads
+	60, # keep idle threads for this long before killing them
+	TimeUnit::SECONDS,
+	LinkedBlockingQueue.new
+)
+
+allSyncJobs=[]
+brokenUp.length.times do |i|
+	logmsg("Spawning sync thread #{i+1}/#{thrct}") if ( (i+1)%10==0 || (i+1)==thrct )
+	thisjob=FutureTask.new( Syncer.new(srcbucket,dstbucket,brokenUp[i],usemd5) )
+	executor.execute(thisjob)
+	allSyncJobs << thisjob	
+end
+
+allSyncJobs.each do |t|
+	t.get
+end
+
+logmsg("ENDED: Sync completed (Check-MD5=#{usemd5}) in #{(Time.now-tsnow).round(3)}secs")
+executor.shutdown()
+
